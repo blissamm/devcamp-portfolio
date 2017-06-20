84b404803e6ca1724a0dc75434d7ba707ca8dfde
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 18:04:59 +0000

    Updated status for enum

--- devcamp_portfolio/db/schema.rb
+++ devcamp_portfolio/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620162032) do
+ActiveRecord::Schema.define(version: 20170620174025) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.integer  "status",     default: 0
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
