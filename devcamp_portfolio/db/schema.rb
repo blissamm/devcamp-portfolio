38c56c2e099c33247dd84aa9180f84f880eda9e5
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:09:44 +0000

    Updating status

--- devcamp_portfolio/db/schema.rb
+++ devcamp_portfolio/db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 20170620174025) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "slug"
-    t.integer  "status",     default: 0
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+     t.integer  "status",     default: 0
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
