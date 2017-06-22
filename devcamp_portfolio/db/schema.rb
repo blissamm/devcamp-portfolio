bc9c6fef91db670e8733196c633070a400545910
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:56:33 +0000

    Integrated database relationship between topics and blog posts

--- devcamp_portfolio/db/schema.rb
+++ devcamp_portfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620174025) do
+ActiveRecord::Schema.define(version: 20170622143801) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170620174025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
-     t.integer  "status",     default: 0
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.integer  "status"
+    t.integer  "topic_id"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+    t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -28,10 +30,10 @@ ActiveRecord::Schema.define(version: 20170620174025) do
     t.string   "sluggable_type", limit: 50
     t.string   "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
   end
 
   create_table "portfolios", force: :cascade do |t|
@@ -49,6 +51,12 @@ ActiveRecord::Schema.define(version: 20170620174025) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
