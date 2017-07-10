eede73796f3fbd5f60c270c4c391867676b3382d
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:51:12 +0000

    making me commit my change

--- devcamp_portfolio/app/controllers/application_controller.rb
+++ devcamp_portfolio/app/controllers/application_controller.rb
@@ -4,13 +4,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-
-before_action :set_copyright
-
-  def set_copyright
-  @copyright = DevcampViewTool::Renderer.copyright 'Ammon Bliss', 'All right reserved'
   
+ before_action :set_copyright 
+ 
+  def set_copyright
+   @copyright = DevcampViewTool::Renderer.copyright 'Ammon Bliss', 'All Rights Reservered'
   end
 end
-
