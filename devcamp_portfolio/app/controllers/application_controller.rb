24a0edbe292da39773e7a84e23b9a37f088d39d8
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:11:21 +0000

    :)

--- devcamp_portfolio/app/controllers/application_controller.rb
+++ devcamp_portfolio/app/controllers/application_controller.rb
@@ -8,14 +8,6 @@ class ApplicationController < ActionController::Base
  before_action :set_copyright 
  
   def set_copyright
-   @copyright = DevcampViewTool::Renderer.copyright 'Ammon Bliss', 'All Rights Reservered'
+   @copyright = AmmonViewTool::Renderer.copyright 'Ammon Bliss', 'All Rights Reservered'
   end
 end
-module DevcampViewTool
-  class Renderer 
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end 
-  end 
-end
- 
